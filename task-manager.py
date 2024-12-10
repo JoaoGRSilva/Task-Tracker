163a747d6a1df628d12a8f5ba6a7d2b95732202e
JoaoGRSilva <joaogabriel975@hotmail.com>
2024-12-10 11:34:14 -0300

    first structure

--- task-manager.py
+++ task-manager.py
@@ -1,10 +1,28 @@
 import json
+from typing import Optional
+from datetime import datetime
 
-def deleteTask():
-    pass
 
-def updateTask():
-    pass
-
-def addTask():
-    pass
+class Task:
+    _id_counter = 0 
+    
+    def __init__(self,
+                 description: str, 
+                 status: str ="todo",
+                 updatedAt: Optional[datetime] = None):
+        Task._id_counter += 1
+        self.id = Task._id_counter
+        self.description = description
+        self.status = status
+        self.createdAt = datetime.now()
+        self.updatedAt = updatedAt or self.createdAt
+        
+    def delete_task():
+        pass
+    
+    def update_task():
+        pass
+    
+    @classmethod
+    def create_task(cls, descrption: str, status: str = "todo") -> "Task":
+        return cls(descrption=descrption, status=status)
