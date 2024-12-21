1eb4a0ee16b490ce46e51aeb53f1b46d75fd1489
Joao <joaogabriel975@hotmail.com>
2024-12-21 10:56:09 -0300

    start of CRUD

--- task-manager.py
+++ task-manager.py
@@ -1,28 +1,60 @@
-import json
-from typing import Optional
+from connector import create_connection,get_cursor
 from datetime import datetime
 
+class TaskManager:
 
-class Task:
-    _id_counter = 0 
-    
-    def __init__(self,
-                 description: str, 
-                 status: str ="todo",
-                 updatedAt: Optional[datetime] = None):
-        Task._id_counter += 1
-        self.id = Task._id_counter
-        self.description = description
-        self.status = status
-        self.createdAt = datetime.now()
-        self.updatedAt = updatedAt or self.createdAt
+    status = ("todo", "in-progress", "done")
+
+    def __init__(self):
+        self.connection = create_connection()
+        if self.connection:
+            self.cursor = get_cursor(self.connection)
+        else:
+            raise Exception("Failed to connect to the database")
         
-    def delete_task():
-        pass
-    
-    def update_task():
-        pass
-    
-    @classmethod
-    def create_task(cls, descrption: str, status: str = "todo") -> "Task":
-        return cls(descrption=descrption, status=status)
+    def create_task(self, description, status: str="todo"):
+        created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        update_at = created_at
+
+        add_query = (
+            "INSET INTO tasks (description, status, created_at, updated_at) "
+            "VALUES (%s, %s, %s, %s)"
+        )
+        task_data = (description, status, created_at, update_at)
+
+        try:
+            self.cursor.execute(add_query, task_data)
+            self.connection.commit()
+            print("Task created successfully.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def update_task(self, id, description):
+        update_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        update_query = (
+            "UPDATE tasks SET description= %s, update_at = %s WHERE id= %s"
+        )
+
+        update_data = (description, update_at, id)
+
+        try:
+            self.cursor.execute(update_query, update_data)
+            self.connection.commit()
+            print("Task update sucessffully.")
+        except Exception as e:
+            print(F"Error: {e}")
+
+    def delete_task(self, id):
+        delete_query = (
+            "DELETE FROM tasks WHERE id = %d"
+        )
+
+        try:
+            self.cursor.execute(delete_query, id)
+            self.connection.commit()
+            print("Task deleted successfully.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def 
